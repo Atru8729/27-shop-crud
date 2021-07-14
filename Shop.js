b1595abefd85ec6e14733799e2ecf2f33b63e891
Atru8729 <a.truskauskas1@gmail.com>
2021-07-14 18:46:30 +0300

    index

--- Shop.js
+++ Shop.js
@@ -1,6 +1,51 @@
 class Shop {
     constructor() {
     }
+
+    intro() {
+
+    }
+
+    addItem() {
+
+    }
+
+    items() {
+
+    }
+
+    updatePrice() {
+
+    }
+
+    createCart() {
+
+    }
+
+    addItemToCart() {
+
+    }
+
+    order() {
+
+    }
+
+    orderPrice() {
+
+    }
+
+    removeItem() {
+
+    }
+
+    pay() {
+
+    }
+
+    shopSummary() {
+
+    }
+
 }
 
 module.exports = Shop;
